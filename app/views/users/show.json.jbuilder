77ba9d04fa66bebe815b58b65d50cefa9bb86e8a
samuelakerman <samsk66@gmail.com>
2017-03-14 03:34:32 -0400

    toy app

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :email, :some_num, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   get 'static_pages/prueba'
 
-  root 'application#hello'
+  root 'users#index'
 end
